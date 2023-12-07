6d82076859941bac6c4d370776775f6c564a700c
uma <chiderovauma@gmail.com>
2023-12-07 15:51:55 +0600

    Added the "attribute" parameter type check.

--- helpers/secondary_functions.py
+++ helpers/secondary_functions.py
@@ -1,4 +1,5 @@
-def validate_attribute(attribute: int, min_val: int, max_val: int or float, message: str) -> None:
-    bool_val = min_val <= attribute <= max_val
+def validate_attribute(attribute: int or list, min_val: int, max_val: int or float, message: str) -> None:
+    validate_data = len(attribute) if isinstance(attribute, list) else attribute
+    bool_val = min_val <= validate_data <= max_val
     if bool_val is False:
         raise ValueError(message)
