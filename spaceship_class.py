ce766439a82b3a32802f18bf8f1e7e41d3ff6b50
uma <chiderovauma@gmail.com>
2023-12-07 16:45:44 +0600

    Implemented a method for setting mounted armor, taking into account the free space in the slot and the free space on the ship.

--- spaceship_class.py
+++ spaceship_class.py
@@ -1,18 +1,20 @@
+from equipments_classes import Weapon, Armor, Navigator
+from helpers.custom_exceptions import FreeSlotError, TotalVolumeError
 from helpers.info_messages import spaceship_data_messages
 from helpers.secondary_functions import validate_attribute
 
 
 class Spaceship:
     def __init__(self, spaciousness, accuracy, slots_for_armor, slots_for_weapons, slot_for_navigation_devices):
-        self.validator(args=[spaciousness, accuracy, slots_for_armor, slots_for_weapons, slot_for_navigation_devices])
+        parameters = [spaciousness, accuracy, slots_for_armor, slots_for_weapons, slot_for_navigation_devices]
+        self.validator(args=parameters)
         self.spaciousness = spaciousness
         self.accuracy = accuracy
         self.health = 1000
         self.slots_for_armor = slots_for_armor
         self.slots_for_weapons = slots_for_weapons
         self.slot_for_navigation_devices = slot_for_navigation_devices
-        self.defence = (1 / (self.spaciousness *  len(self.slots_for_armor))) * 10 ** 4
-
+        self.defence = (1 / (self.spaciousness * len(self.slots_for_armor))) * 10 ** 4
 
     @staticmethod
     def validator(args):
@@ -41,3 +43,19 @@ class Spaceship:
                            min_val=1,
                            max_val=2,
                            message=spaceship_data_messages.get("slot_for_navigation_devices_error_message"))
+
+    def set_armor(self, armor_object):
+        if len(self.slots_for_armor) == 3:
+            raise FreeSlotError()
+        self.check_spaceship_capacity(equipment=armor_object)
+        self.slots_for_armor.append(armor_object)
+
+    def check_spaceship_capacity(self, equipment):
+        if sum([self.get_total_occupied_volume_of_equipment(), equipment.taken_capacity]) >= self.spaciousness:
+            raise TotalVolumeError()
+
+    def get_total_occupied_volume_of_equipment(self):
+        armors_taken_capacity_amount = sum([armor.taken_capacity for armor in self.slots_for_armor])
+        weapons_taken_capacity_amount = sum([weapon.taken_capacity for weapon in self.slots_for_weapons])
+        navigation_devices_taken_capacity_amount = sum([navigation_device.taken_capacity for navigation_device in self.slot_for_navigation_devices])
+        return sum([armors_taken_capacity_amount, weapons_taken_capacity_amount, navigation_devices_taken_capacity_amount])
