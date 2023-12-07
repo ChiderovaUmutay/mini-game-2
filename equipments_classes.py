1800f8c8ebb61980e6c06b1d400c323b0b753e4b
uma <chiderovauma@gmail.com>
2023-12-07 17:21:19 +0600

    Created a “variables.py” file, where moved all the magic numbers from the “equipments_classes.py” file.

--- equipments_classes.py
+++ equipments_classes.py
@@ -4,27 +4,29 @@ from helpers.custom_exceptions import EquipmentWornOutError
 from helpers.info_messages import equipment_data_messages, weapon_data_messages, armor_data_messages, \
     navigator_data_messages
 from helpers.secondary_functions import validate_attribute
+from helpers.variables import wear_condition_values, damage_values, critical_hit_chance_values, MISFIRE_PERCENTAGE, \
+    taken_capacity_values, armor_defence_values, navigator_accuracy_values, ELECTROMAGNETIC_SURGE_PERCENTAGE
 
 
 class Equipment:
     def __init__(self, name: str, taken_capacity: int) -> None:
         validate_attribute(attribute=taken_capacity,
-                           min_val=30,
-                           max_val=100,
+                           min_val=taken_capacity_values.get("min_val"),
+                           max_val=taken_capacity_values.get("max_val"),
                            message=equipment_data_messages.get("taken_capacity_error_message"))
         self.name = name
-        self.wear_condition = 0
+        self.wear_condition = wear_condition_values.get("min_val")
         self.taken_capacity = taken_capacity
 
     def action(self) -> None:
-        if self.wear_condition >= 100:
+        if self.wear_condition >= wear_condition_values.get("max_val"):
             raise EquipmentWornOutError()
         else:
-            self.wear_condition += 10
+            self.wear_condition += wear_condition_values.get("increasing_val")
 
     def calculate_equipment_efficiency(self, equipment: int or float) -> int or float:
-        calculated_damage = equipment - (equipment * self.wear_condition) / 100
-        return calculated_damage
+        calculated_efficiency = equipment - (equipment * self.wear_condition) / 100
+        return calculated_efficiency
 
     def __str__(self) -> None:
         print(equipment_data_messages.get("info_message").format(self.name,
@@ -37,12 +39,12 @@ class Weapon(Equipment):
     def __init__(self, name: str, taken_capacity: int, min_damage: int, critical_hit_chance: int) -> None:
         super().__init__(name, taken_capacity)
         validate_attribute(attribute=min_damage,
-                           min_val=5,
-                           max_val=40,
+                           min_val=damage_values.get("min_val"),
+                           max_val=damage_values.get("max_val"),
                            message=weapon_data_messages.get("min_damage_error_message"))
         validate_attribute(attribute=critical_hit_chance,
-                           min_val=1,
-                           max_val=70,
+                           min_val=critical_hit_chance_values.get("min_val"),
+                           max_val=critical_hit_chance_values.get("max_val"),
                            message=weapon_data_messages.get("critical_hit_chance_error_message"))
         self.min_damage = min_damage
         self.critical_hit_chance = critical_hit_chance
@@ -53,7 +55,7 @@ class Weapon(Equipment):
         if damage <= self.critical_hit_chance:
             critical_damage = (self.max_damage * 40) / 100
             return self.get_calculated_damage(critical_damage)
-        elif damage <= 15:
+        elif damage <= MISFIRE_PERCENTAGE:
             return 0
         else:
             return self.get_calculated_damage(damage)
@@ -75,8 +77,8 @@ class Armor(Equipment):
     def __init__(self, name: str, taken_capacity: int, defence: int):
         super().__init__(name, taken_capacity)
         validate_attribute(attribute=defence,
-                           min_val=1,
-                           max_val=10,
+                           min_val=armor_defence_values.get("min_val"),
+                           max_val=armor_defence_values.get("max_val"),
                            message=armor_data_messages.get("defense_error_message"))
         self.defence = defence
 
@@ -95,14 +97,14 @@ class Navigator(Equipment):
     def __init__(self, name: str, taken_capacity: int, accuracy: int):
         super().__init__(name, taken_capacity)
         validate_attribute(attribute=accuracy,
-                           min_val=5,
-                           max_val=40,
+                           min_val=navigator_accuracy_values.get("min_val"),
+                           max_val=navigator_accuracy_values.get("max_val"),
                            message=navigator_data_messages.get("accuracy_error_message"))
         self.accuracy = accuracy
 
     def action(self) -> int or float:
         electromagnetic_surge_probability = randint(1, 100)
-        if electromagnetic_surge_probability <= 20:
+        if electromagnetic_surge_probability <= ELECTROMAGNETIC_SURGE_PERCENTAGE:
             self.accuracy /= 2
         calculated_accuracy = self.calculate_equipment_efficiency(self.accuracy)
         return calculated_accuracy
---
+++ helpers/variables.py
@@ -0,0 +1,26 @@
+wear_condition_values = {
+    "min_val": 0,
+    "max_val": 100,
+    "increasing_val": 10
+}
+
+taken_capacity_values = {
+    "min_val": 30,
+    "max_val": 100
+}
+
+armor_defence_values = {
+    "min_val": 1,
+    "max_val": 10
+}
+
+navigator_accuracy_values = {
+    "min_val": 5,
+    "max_val": 40
+}
+
+damage_values = {"min_val": 5, "max_val": 40}
+critical_hit_chance_values = {"min_val": 1, "max_val": 70}
+
+MISFIRE_PERCENTAGE = 15
+ELECTROMAGNETIC_SURGE_PERCENTAGE = 20
