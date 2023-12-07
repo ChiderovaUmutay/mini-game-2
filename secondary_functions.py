d87ef102e0d37c3db4c561941776dc19320e82c7
uma <chiderovauma@gmail.com>
2023-12-07 14:37:27 +0600

    Type hints have been added to the "validate_attribute()" function.

--- secondary_functions.py
+++ secondary_functions.py
@@ -1,4 +1,4 @@
-def validate_attribute(attribute, min_val, max_val, message):
+def validate_attribute(attribute: int, min_val: int, max_val: int or float, message: str) -> None:
     bool_val = min_val <= attribute <= max_val
     if bool_val is False:
         raise ValueError(message)
