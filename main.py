b68a4ea8a5d24fb5fed278825723490f9715db74
uma <chiderovauma@gmail.com>
2023-12-07 11:34:34 +0600

    A class “Weapon()” was created, which is inherited from the base class “Equipment()”.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import uniform
+
+
 class EquipmentWornOutError(Exception):
     pass
 
@@ -33,3 +36,42 @@ class Equipment:
                                     f"Equipment wear condition: {self.wear_condition}\n" \
                                     f"Equipment taken capacity: {self.taken_capacity}"
         print(equipment_characteristics)
+
+
+class Weapon(Equipment):
+
+    def __init__(self, name, taken_capacity, min_damage, critical_hit_chance, wear_condition=0):
+        super().__init__(name, taken_capacity, wear_condition=wear_condition)
+        self.validate_attribute(attribute=min_damage,
+                                 min_val=5,
+                                 max_val=40,
+                                 message="Minimum damage range should be from 5 to 40")
+        self.validate_attribute(attribute=critical_hit_chance,
+                                min_val=1,
+                                max_val=70,
+                                message="Critical damage range should be from 1 to 70")
+        self.min_damage = min_damage
+        self.critical_hit_chance = critical_hit_chance
+        self.max_damage = self.min_damage + (self.min_damage * 40) / 100
+
+    def action(self):
+        damage = round(uniform(self.min_damage, self.max_damage))
+        if damage <= self.critical_hit_chance:
+            critical_damage = (self.max_damage * 40) / 100
+            return self.get_calculated_damage(critical_damage)
+        elif damage <= 15:
+            return 0
+        else:
+            return self.get_calculated_damage(damage)
+
+    def __str__(self):
+        super().__str__()
+        damage_info = f"Minimum damage: {self.min_damage}\n" \
+                      f"Maximum damage: {self.max_damage}\n" \
+                      f"Critical hit chance: {self.critical_hit_chance}"
+        print(damage_info)
+
+    def get_calculated_damage(self, damage):
+        calculated_damage = self.calculate_damage_taking_into_account_wear(damage)
+        super().action()
+        return calculated_damage
