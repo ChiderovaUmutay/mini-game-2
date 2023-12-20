487829288d8fdbc146e70ed9dff07535d3bbed1f
uma <chiderovauma@gmail.com>
2023-12-20 14:19:07 +0600

    In all error messages, instead of numbers, I inserted dictionaries with maximum and minimum parameter values.

--- helpers/info_messages.py
+++ helpers/info_messages.py
@@ -1,15 +1,29 @@
-from helpers.variables import drone_efficiency_values
+from helpers.variables import drone_efficiency_values, \
+    taken_capacity_values, \
+    weapon_damage_values, \
+    weapon_critical_hit_values, \
+    armor_defence_values, \
+    navigator_accuracy_values, \
+    ship_spaciousness_values, \
+    ship_accuracy_values, \
+    ship_armor_slot_values, \
+    ship_weapon_slot_values, \
+    ship_navigation_slot_values, \
+    ship_drone_slot_values
 
 equipment_data_messages = {
-    "taken_capacity_error_message": "The number of units of occupied volume should be from 30 to 100",
+    "taken_capacity_error_message": f"The number of units of occupied volume should be from "
+                                    f"{taken_capacity_values.get('min_val')} to {taken_capacity_values.get('max_val')}",
     "info_message": "\nEquipment name: {}\n"
                     "Wear condition of equipment: {}\n"
                     "Taken capacity of equipment: {}\n"
 }
 
 weapon_data_messages = {
-    "min_damage_error_message": "The number of units of minimum damage should be from 5 to 40",
-    "critical_hit_chance_error_message": "The number of units of critical chance should be from 1 to 70",
+    "min_damage_error_message": f"The number of units of minimum damage should be from "
+                                f"{weapon_damage_values.get('min_val')} to {weapon_damage_values.get('max_val')}",
+    "critical_hit_chance_error_message": f"The number of units of critical chance should be from "
+                                         f"{weapon_critical_hit_values.get('min_val')} to {weapon_critical_hit_values.get('max_val')}",
     "info_message": 'The weapon type is "{}"\n'
                     "The number of weapon minimum damage units: {}\n"
                     "The number of weapon maximum damage units: {}\n"
@@ -17,13 +31,15 @@ weapon_data_messages = {
 }
 
 armor_data_messages = {
-    "defense_error_message": "The range of the number of protection units should be from 1 to 10",
+    "defense_error_message": f"The range of the number of defence units should be from "
+                             f"{armor_defence_values.get('min_val')} to {armor_defence_values.get('max_val')}",
     "info_message": 'The armor type is "{}"\n'
                     "The number of armor protection units: {}\n"
 }
 
 navigator_data_messages = {
-    "accuracy_error_message": "The range of the number of accuracy units should be from 5 to 40",
+    "accuracy_error_message": f"The range of the number of accuracy units should be from "
+                              f"{navigator_accuracy_values.get('min_val')} to {navigator_accuracy_values.get('max_val')}",
     "info_message": "The number of navigator accuracy units: {}\n"
 }
 
@@ -34,12 +50,18 @@ drone_data_messages = {
 }
 
 spaceship_data_messages = {
-    "spaciousness_error_message": "The range of number of spaceship spaciousness units should be from 300 to 1000",
-    "accuracy_error_message": "The range of the number of spaceship accuracy units must be from 0 to 5",
-    "armor_slot_error_message": "The number of slots for the spaceship's hinged armor should be from 1 to 3",
-    "weapon_slot_error_message": "The number of weapon slots for a spaceship should be from 1 to 3",
-    "navigation_slot_error_message": "The number of slots for navigation devices for a spaceship should be from 1 to 2",
-    "drone_slot_error_message": "The number of slots for healing drones for a spaceship should be from 0 to 2"
+    "spaciousness_error_message": f"The range of number of spaceship spaciousness units should be from "
+                                  f"{ship_spaciousness_values.get('min_val')} to {ship_spaciousness_values.get('max_val')}",
+    "accuracy_error_message": f"The range of the number of spaceship accuracy units must be from "
+                              f"{ship_accuracy_values.get('min_val')} to {ship_accuracy_values.get('max_val')}",
+    "armor_slot_error_message": f"The number of slots for the spaceship's hinged armor should be from "
+                                f"{ship_armor_slot_values.get('min_qty')} to {ship_armor_slot_values.get('max_qty')}",
+    "weapon_slot_error_message": f"The number of weapon slots for a spaceship should be from"
+                                 f"{ship_weapon_slot_values.get('min_qty')} to {ship_weapon_slot_values.get('max_qty')}",
+    "navigation_slot_error_message": f"The number of slots for navigation devices for a spaceship should be from "
+                                     f"{ship_navigation_slot_values.get('min_qty')} to {ship_navigation_slot_values.get('max_qty')}",
+    "drone_slot_error_message": f"The number of slots for healing drones for a spaceship should be from "
+                                f"{ship_drone_slot_values.get('min_qty')} to {ship_drone_slot_values.get('max_qty')}"
 }
 
 equipment_header_characteristic = {
